80ee04fb26bcf4637da5b576e63e8c0572298633
JabKh <jaber.khaloua@hotmail.com>
2024-03-11 16:54:08 +0100

    10

--- App.jsx
+++ App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TodoApp from './components/TodoApp/TodoApp';
 import ContactListModal from './components/Contact/ContactListModal';
 
@@ -11,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Todo" component={TodoApp} options={{ title: 'Todo' }} />
+        <Tab.Screen name="Todo" component={TodoApp} options={{ title: 'TodoList' }} />
         <Tab.Screen name="Contact" component={ContactListModal} options={{ title: 'Contact' }} />
       </Tab.Navigator>
     </NavigationContainer>
